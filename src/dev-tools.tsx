779b2ffd5bd9b9f53fb0bd4b73ffd92673902913
majid_taheri <majid@taheri24.ir>
2018-05-29 10:50:02 +0430

    add some features to dev-tools

--- src/dev-tools.tsx
+++ src/dev-tools.tsx
@@ -1,10 +1,44 @@
 import './dev-tools/devtools-data-form';
 import './dev-tools/devtools-boxes';
+/// <reference path="organicUI.d.ts" />
+OrganicUI.devTools.set('Translate',(dev)=>{
+    OrganicUI.DeveloperBar.topElement=<FabricUI.Dialog onDismiss={()=>{
+        OrganicUI.DeveloperBar.topElement=null;
+        dev.forceUpdate();      
+    }} isOpen={true} dialogContentProps={ {
+     
+        title: 'Translate',
+        subText: 'detected unlocalized text in below lists'
+      } }  >
+ <table className="table is-bordered">
+         {Object.keys(    OrganicUI.i18n.notFounded).map(key=>(
+<tr>
+    <td>{key}</td>
+    <td> <FabricUI.TextField /></td>
+    </tr>
 
+         ))}
+         </table>
+        </FabricUI.Dialog>;
+    
+    dev.forceUpdate();   
+}) ;
+OrganicUI.devTools.set('Reset All Dev Tools',()=>{
+    Array.from(  document.querySelectorAll('.developer-port'))
+        .map(dv=>dv['componentRef'] )
+        .filter(dv=>dv && dv.devElement && dv.forceUpdate).forEach( dv=>{
+            dv.devElement= null;
+            dv.forceUpdate();
+        });
+});
+ 
 document.addEventListener('keydown', e => {
     if (e.key == 'F1') {
+        e.preventDefault();  
+        Array.from(  document.querySelectorAll('.dev-target'))
+            .forEach(element=>element.classList.remove('dev-target'));
+
        
-        e.preventDefault();
         OrganicUI.DevFriendlyPort.developerFriendlyEnabled =
             !OrganicUI.DevFriendlyPort.developerFriendlyEnabled;
         const componentRefs =
