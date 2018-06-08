5d7c845aaa725cdb711dee9bec584f0e6fd7f666
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2018-06-08 14:34:06 +0430

    some refactor & server-side hot-module-loader

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-
+const recursiveWatch = require('recursive-watch');
 const shelljs = require('shelljs');
 const child = shelljs.exec('npm run build:watch', { async: true });
-child.stdout.on('data', console.log);
+child.stdout.on('data', data => {
+    notifyToAllUserForFileChanging('reloadAllTargetedItems');
+    console.log(data);
+});
 const child2 = shelljs.exec('npm run build:sass:watch', { async: true });
 child2.stdout.on('data', console.log);
 const jsonServer = require('json-server');
 const path = require('path');
 const express = require('express');
- 
+
 const router = jsonServer.router('db.json');
 //const reload = require('require-reload')(require);
 const server = express();
@@ -22,30 +25,21 @@ const assetsPath = path.join(__dirname, '../assets');
 server.use('/assets/domain', express.static(path.join(__dirname, '..', 'assets', 'domain')));
 server.use('/assets', express.static(assetsPath));
 server.get('/', (req, res) => res.redirect('/view/dashboard'));
-const allWebSockets = [];
-const notifyToAllUserForFileChanging = () => console.warn('notifyToAllUserForFileChanging');
+let allWebSockets = [];
+const notifyToAllUserForFileChanging = msg => {
+    allWebSockets.forEach(ws => {
+        ws.readyState == 1 && ws.send(JSON.stringify(msg));
+    }
+    );
+}
 server.ws('/watch', function (ws, req) {
     ws.on('message', function (msg) {
         console.log(msg);
     });
     console.log('socket', req.testing);
+    allWebSockets = allWebSockets.filter(ws => ws.readyState == 1);
+    allWebSockets.push(ws);
 });
 server.listen(3000);
 const sourceDir = path.join(process.cwd(), 'src');
-const DirectoryWatcher = require('directory-watcher');
-DirectoryWatcher.create(sourceDir, function (err, watcher) {
-    /* watcher.on('change', notifyToAllUserForFileChanging);
-     watcher.on('delete', notifyToAllUserForFileChanging);
-     watcher.on('add', notifyToAllUserForFileChanging);*/
-    watcher.once('change', function (files) {
-        console.log('will fire once');
-    });
-
-    watcher.on('delete', function (files) {
-        console.log('%s deleted', files);
-    });
-
-    watcher.on('add', function (files) {
-        console.log('%s added', files);
-    });
-});
+recursiveWatch(sourceDir, files => notifyToAllUserForFileChanging('serverChanged')); 
--- src/lib/organic-box.tsx
+++ src/lib/organic-box.tsx
@@ -12,8 +12,8 @@ import { IDataListProps, DataList } from './data-list';
 import { DataForm } from './data-form';
 import { Spinner } from './spinner';
 import { AdvButton, Placeholder } from './ui-kit';
- 
-import { isDevelopmentEnv,DevFriendlyPort } from './developer-friendly';
+
+import { isDevelopmentEnv, DevFriendlyPort } from './developer-friendly';
 const { OverflowSet, SearchBox, DefaultButton, css } = FabricUI;
 
 interface SingleViewBoxState { formData: any; validated: boolean; }
@@ -25,10 +25,11 @@ function loadScript(src) {
 }
 export default class OrganicBox<P, S> extends BaseComponent<P, S> {
     serverChanged(): any {
-        const board = document.querySelector('#dev-server-board');
-        board && board.classList.add('active');
-        board && (board.innerHTML = 'server files is changed, building started...');
-
+        const boards = Array.from(document.querySelectorAll('#dev-server-board'));
+        boards.forEach(board => {
+            board.classList.add('active');
+            board.innerHTML = 'server files is changed, building started...';
+        });
     }
     webSocket: WebSocket;
     static stableState: any;
@@ -47,7 +48,7 @@ export default class OrganicBox<P, S> extends BaseComponent<P, S> {
     constructor(p) {
         super(p);
         if (isDevelopmentEnv()) {
-            try {
+            try { 
                 this.webSocket = new WebSocket(`ws://${location.host}/watch`);
                 this.webSocket.onmessage = ({ data }) => (
                     (data == 'reloadAllTargetedItems' && this.reloadAllTargetedItems()) ||
