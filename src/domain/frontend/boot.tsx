c4ee71f36f54cc037be8133b9d1bd57005bdf80e
majid_taheri <majid@taheri24.ir>
2018-06-09 18:38:12 +0430

    fix OrganicUI.ActionManager.bodyMapper

--- src/domain/frontend/boot.tsx
+++ src/domain/frontend/boot.tsx
@@ -4,7 +4,7 @@ const customMapProps = {
 
 }
 OrganicUI.ActionManager.bodyMapper = ({ url, method, body }) => {
-    if (method != 'GET') return body;
+    if (method != 'GET' || !body) return body;
     for (var key in customMapProps) {
         if (body[key] === undefined) continue;
         body[customMapProps[key]] = body[key];
