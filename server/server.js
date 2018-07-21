1e4379e466f7c4c6d498a8b97ef60071273d08c3
majid_taheri <majid@taheri24.ir>
2018-07-21 11:40:50 +0430

    remove json-server

--- server/server.js
+++ server/server.js
@@ -5,32 +5,35 @@ const fs = require('fs');
 
 
 const checkFileExists = s => new Promise(r => fs.access(s, fs.F_OK, e => r(!e)))
-Promise.all(
-    ['../src/domain/domain.tsx'].map(filePath => checkFileExists(path.join(__dirname, filePath)))).
-    then(([domainDevMode]) => {
-        if (!domainDevMode) return;
-        const child = shelljs.exec('npm run build:watch', { async: true });
-        child.stdout.on('data', data => {
-            if ((data || '').includes('size'))
-                setTimeout(() => notifyToAllUserForFileChanging('reloadAllTargetedItems'), 1000);
-            console.log(data);
-        });
-        shelljs.exec('npm run build:sass', { async: true });
-        const child2 = shelljs.exec('npm run build:sass:watch', { async: true });
-        child2.stdout.on('data', console.log);
-    });
+const fileExists = filePath => {
+    const filePathArray = filePath instanceof Array ? filePath : [filePath];
+    try {
+        for (const fileName of filePathArray) {
+            const r = fs.statSync(fileName);
+            if (!r.size) return false;
+        }
+        return true;
+    }
+    catch(exc){
+        console.log(exc);
+        return false;
+    }
+}
+const child = shelljs.exec('npm run build:watch', { async: true });
+child.stdout.on('data', data => {
+    if ((data || '').includes('size'))
+        setTimeout(() => notifyToAllUserForFileChanging('reloadAllTargetedItems'), 1000);
+    console.log(data);
+});
+if (fileExists(path.join(process.cwd(), './src/styles/all.scss'))) {
+    shelljs.exec('npm run build:sass', { async: true });
+    const child2 = shelljs.exec('npm run build:sass:watch', { async: true });
+    child2.stdout.on('data', console.log);
+}
 const express = require('express');
-
-const jsonServer = require('json-server');
-const router = jsonServer.router('database.json');
 const server = express();
-var expressWs = require('express-ws')(server);
-
-const bodyParser = require('body-parser');
-
-server.use('/api', bodyParser.json(), router)
-
-server.use('/view', (req, res) => res.sendFile(path.join(__dirname, '../assets/single-page-app.html')));
+require('express-ws')(server);
+server.use('/view', (req, res) => res.sendFile(path.join(process.cwd(), 'assets/single-page-app.html')));
 const assetsPath = path.join(__dirname, '../assets');
 server.use('/assets/domain', express.static(path.join(__dirname, '..', 'assets', 'domain')));
 server.use('/assets', express.static(assetsPath));
