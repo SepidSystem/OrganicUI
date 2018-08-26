5eb463c4f0788b17ca2574d15ac3f3a40154b1ff
majid_taheri <majid@taheri24.ir>
2018-08-26 13:52:56 +0430

    update dashboard interval

--- front-end/src/lib/reinvent/dashboard-reinvent.tsx
+++ front-end/src/lib/reinvent/dashboard-reinvent.tsx
@@ -5,9 +5,30 @@ function classFactory<TData, TState=any>(options: OrganicUi.IDashboardWidgetOpti
     OrganicUi.IDashboardWidgetReinvent<TData, TState> {
     const chainMethods = ['paramInitializer', 'dataLoader', 'dataRenderer', 'size'];
     const AClass = reinvent.baseClassFactory({ chainMethods, className: 'dashboard-widget' });
+    const reactClass: typeof React.Component = AClass;
+    if (options.interval) {
 
+        reactClass.prototype.componentWillMount = function () {
+            const tick = () => {
+                const param = AClass.applyChain('paramInitializer');
+
+                Utils.toPromise(AClass.applyChain('dataLoader', param))
+                    .then(data => {
+                        this.repatch({ data })
+                        if (this['timerActivate'])
+                            setTimeout(tick, options.interval);
+                        return data;
+                    });
+            }
+            setTimeout(tick, options.interval);
+        }
+        reactClass.prototype.componentWillUnmount = function () {
+            delete this['timerActivate'];
+        }
+    }
     AClass.afterConsturct = function () {
         const param = AClass.applyChain('paramInitializer');
+        this['timerActivate'] = 1;
         const loader = Utils.toPromise(AClass.applyChain('dataLoader', param))
             .then(data => this.repatch({ data }));
         Object.assign(this.state, { data: loader, param });
@@ -20,7 +41,7 @@ function classFactory<TData, TState=any>(options: OrganicUi.IDashboardWidgetOpti
             param, data,
             repatch: target.repatch.bind(target),
             runAction: target.runAction.bind(this),
-            root:target.refs.root ,
+            root: target.refs.root,
             subrender: target.subrender.bind(target),
             showModal: target.showModal.bind(target)
         };
