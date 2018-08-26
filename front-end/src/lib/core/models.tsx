0855f61dc52490686f4c0c36e26addc5c540a467
majid_taheri <majid@taheri24.ir>
2018-08-26 13:53:30 +0430

    add selectionLink for support single-view menu high-light in side-bar

--- front-end/src/lib/core/models.tsx
+++ front-end/src/lib/core/models.tsx
@@ -1,4 +1,5 @@
 export class Menu implements OrganicUi.IMenu {
+    selectionLink: string;
     constructor(public id: number,
         public title: string,
         public routerLink: string,
@@ -6,5 +7,8 @@ export class Menu implements OrganicUi.IMenu {
         public icon: string,
         public target: string,
         public hasSubMenu: boolean,
-        public parentId: number) { }
+        public parentId: number) {
+
+        this.selectionLink = routerLink && routerLink.endsWith('s') ? routerLink.substring(0, routerLink.length - 1) : routerLink;
+    }
 }
